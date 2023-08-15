d268f81c967ba1f9093df2213de70c68be6f5734
Phatjira Rungsakullikit <pjr_oh@hotmail.com>
2023-08-15 16:50:41 +0200

    Add characters left

--- javascript/form.js
+++ javascript/form.js
@@ -1,81 +1,3 @@
-// const bookmarkButtons = document.querySelectorAll(
-//   '[data-js="bookmark-button"]'
-// );
-
-// bookmarkButtons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     button.classList.toggle("dark");
-//   });
-// });
-
-// const showAnswerButtons = document.querySelectorAll(
-//   '[data-js="show-answer-button"]'
-// );
-// const answers = document.querySelectorAll('[data-js="answer"]');
-
-// showAnswerButtons.forEach((button, index) => {
-//   button.addEventListener("click", () => {
-//     answers[index].classList.toggle("visible");
-//   });
-// });
-
-// const form = document.querySelector('[data-js="form"]');
-// const main = document.querySelector('[data-js="main"]');
-
-// function addQuestion(question, answer, tag) {
-//   const section = document.createElement("section");
-//   section.classList.add("question");
-
-//   const divBookmark = document.createElement("div");
-//   divBookmark.classList.add("bookmarkRibbon");
-//   divBookmark.dataset.js = "bookmark-button";
-
-//   const headline = document.createElement("h2");
-//   headline.innerText = question;
-
-//   const divButtonContainer = document.createElement("div");
-//   divButtonContainer.classList.add("button-container");
-
-//   const button = document.createElement("button");
-//   button.classList.add("show-answer");
-//   button.dataset.js = "show-answer-button";
-//   button.textContent = "Show Answer";
-
-//   const divAnswer = document.createElement("div");
-//   divAnswer.classList.add("answer");
-//   divAnswer.dataset.js = "answer";
-//   divAnswer.innerText = answer;
-//   divAnswer.addEventListener("click", () => {
-//     answer.classList.toggle("visible");
-//   });
-
-//   const divTagContainer = document.createElement("div");
-//   divTagContainer.classList.add("tag-container");
-
-//   const paragraph = document.createElement("p");
-//   paragraph.classList.add("tag");
-//   paragraph.innerText = `#${tag}`;
-
-//   main.appendChild(section);
-//   section.append(
-//     divBookmark,
-//     headline,
-//     divButtonContainer,
-//     divAnswer,
-//     divTagContainer
-//   );
-//   divButtonContainer.appendChild(button);
-//   divTagContainer.appendChild(paragraph);
-// }
-
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const yourQuestion = event.target.question.value;
-//   const yourAnswer = event.target.answer.value;
-//   const tag = event.target.tag.value;
-//   addQuestion(yourQuestion, yourAnswer, tag);
-// });
-
 const form = document.querySelector('[data-js="form"]');
 const main = document.querySelector('[data-js="main"]');
 
@@ -146,4 +68,16 @@ form.addEventListener("submit", (event) => {
   const yourAnswer = event.target.answer.value;
   const tag = event.target.tag.value;
   addQuestion(yourQuestion, yourAnswer, tag);
+});
+
+const messageInput = document.querySelectorAll('[data-js="message"]');
+const charactersLeftElement = document.querySelector(
+  '[data-js="remaining-characters"]'
+);
+
+messageInput.forEach((message) => {
+  message.addEventListener("input", (event) => {
+    charactersLeftElement.textContent =
+      150 - parseInt(event.target.value.length, 10);
+  });
 });
